135eb18a4d891d766fa8c3925fd140b11ecf5a0b
vjabuilds <jovan.vjestica@outlook.com>
2022-10-21 13:44:58 +0200

    Initialized project

---
+++ src/main/java/dev/vjabuilds/datawavesexperimenter/DatawavesExperimenterApplication.java
@@ -0,0 +1,13 @@
+package dev.vjabuilds.datawavesexperimenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawavesExperimenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatawavesExperimenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vjabuilds/datawavesexperimenter/DatawavesExperimenterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vjabuilds.datawavesexperimenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawavesExperimenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
