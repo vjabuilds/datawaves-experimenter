90efbc26b2ffa6c38e861dafd3fe80e8b1863bce
vjabuilds <jovan.vjestica@outlook.com>
2022-10-21 17:57:28 +0200

    Added import for Order annotation

--- src/main/java/dev/vjabuilds/datawavesexperimenter/models/db_init/DatasetInitializer.java
+++ src/main/java/dev/vjabuilds/datawavesexperimenter/models/db_init/DatasetInitializer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import dev.vjabuilds.datawavesexperimenter.models.Dataset;
