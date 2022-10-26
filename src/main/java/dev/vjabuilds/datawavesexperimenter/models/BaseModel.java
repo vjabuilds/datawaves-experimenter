819548ef64eca56e343d93ed204edd16d504155d
vjabuilds <jovan.vjestica@outlook.com>
2022-10-26 20:49:52 +0200

    Updated BaseModel to reflect timezone changes

--- src/main/java/dev/vjabuilds/datawavesexperimenter/models/BaseModel.java
+++ src/main/java/dev/vjabuilds/datawavesexperimenter/models/BaseModel.java
@@ -1,19 +1,19 @@
 package dev.vjabuilds.datawavesexperimenter.models;
 
-import java.util.Date;
+import java.time.ZonedDateTime;
 
 import lombok.Data;
 
 @Data
 public abstract class BaseModel {
-    protected Date created;
-    protected Date updated;
-    protected Date deleted;
+    protected ZonedDateTime created;
+    protected ZonedDateTime updated;
+    protected ZonedDateTime deleted;
 
     public BaseModel()
     {
-        created = new Date();
-        updated = new Date();
+        created = ZonedDateTime.now();
+        updated = ZonedDateTime.now();
         deleted = null;
     }
 }
