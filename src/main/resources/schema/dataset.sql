82dbb1f6dd6a031396e722546a77680514270f91
vjabuilds <jovan.vjestica@outlook.com>
2022-10-26 20:49:18 +0200

    Updated models to use timezoned timestamps

--- src/main/resources/schema/dataset.sql
+++ src/main/resources/schema/dataset.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS public.dataset (
     "type" varchar(255) NULL,
 	"parent_dataset_id" int NULL,
 	"parent_pipeline_id" int NULL,
-    "created" timestamp NULL,
-    "updated" timestamp NULL,
-    "deleted" timestamp NULL,
+    "created" timestamp with time zone NULL,
+    "updated" timestamp with time zone  NULL,
+    "deleted" timestamp with time zone  NULL,
 	CONSTRAINT dataset_pkey PRIMARY KEY (dataset_id),
 	CONSTRAINT dataset_parent_fkey FOREIGN KEY(parent_dataset_id) REFERENCES public.dataset(dataset_id),
 	CONSTRAINT pipeline_parent_fkey FOREIGN KEY(parent_pipeline_id) REFERENCES public.pipeline(pipeline_id)
--- src/main/resources/schema/pipeline.sql
+++ src/main/resources/schema/pipeline.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS public.pipeline (
 	"pipeline_id" bigserial NOT NULL,
 	"name" varchar(255) NULL,
 	"description" varchar(255) NULL,
-    "created" timestamp NULL,
-    "updated" timestamp NULL,
-    "deleted" timestamp NULL,
+	"created" timestamp with time zone NULL,
+    "updated" timestamp with time zone  NULL,
+    "deleted" timestamp with time zone  NULL,
 	CONSTRAINT pipeline_pkey PRIMARY KEY (pipeline_id)
 );
