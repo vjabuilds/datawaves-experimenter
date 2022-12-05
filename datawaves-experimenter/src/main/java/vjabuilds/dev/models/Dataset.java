d1b226477b1165b49b85d93763be0901a4be1f6f
vjabuilds <jovan.vjestica@outlook.com>
2022-12-05 15:42:32 +0100

    Made dataset into panache entity

--- datawaves-experimenter/src/main/java/vjabuilds/dev/models/Dataset.java
+++ datawaves-experimenter/src/main/java/vjabuilds/dev/models/Dataset.java
@@ -1,13 +1,19 @@
 package vjabuilds.dev.models;
 
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-public class Dataset {
+@Entity
+public class Dataset extends PanacheEntity {
     private Long datasetId;
     private String name;
     private String source;
