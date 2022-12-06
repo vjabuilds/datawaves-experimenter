e72a11913beb2ede428692dad53d3d39838bde7d
vjabuilds <jovan.vjestica@outlook.com>
2022-12-06 18:04:44 +0100

    Added soft delete for datasets

--- datawaves-experimenter/src/main/java/vjabuilds/dev/resources/DatasetResource.java
+++ datawaves-experimenter/src/main/java/vjabuilds/dev/resources/DatasetResource.java
@@ -1,14 +1,19 @@
 package vjabuilds.dev.resources;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import org.jboss.resteasy.reactive.RestPath;
 
 import io.quarkus.hibernate.reactive.panache.Panache;
 import io.smallrye.mutiny.Uni;
@@ -35,5 +40,17 @@ public class DatasetResource {
         return Panache.withTransaction(() -> {
             return datasetRepo.persist(ds).map(x -> Response.status(Status.CREATED).entity(x).build());
         });
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Uni<Response> deleteDataset(@RestPath Long id)
+    {
+        return Panache.withTransaction(() -> 
+            datasetRepo.findById(id).onItem()
+                .ifNotNull().invoke(x -> x.setDeleted(ZonedDateTime.now()))
+            )
+            .onItem().ifNotNull().transform(x -> Response.ok().build())
+            .onItem().ifNull().continueWith(() -> Response.status(404).build());
     }
 }
