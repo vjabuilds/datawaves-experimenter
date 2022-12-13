12c7f16fa37001ce25f5628c9157ed2e48a29fc9
vjabuilds <jovan.vjestica@outlook.com>
2022-12-13 15:11:19 +0100

    Added @ApplicationScoped for MLModelRepo

--- datawaves-experimenter/src/main/java/vjabuilds/dev/repos/MLModelRepo.java
+++ datawaves-experimenter/src/main/java/vjabuilds/dev/repos/MLModelRepo.java
@@ -1,8 +1,11 @@
 package vjabuilds.dev.repos;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
 import vjabuilds.dev.models.MLModel;
 
+@ApplicationScoped
 public class MLModelRepo implements PanacheRepository<MLModel> {
     
 }
