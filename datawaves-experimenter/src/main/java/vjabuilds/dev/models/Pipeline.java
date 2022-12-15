86779b9294c483863799a71e0ea6b0798b419a10
vjabuilds <jovan.vjestica@outlook.com>
2022-12-15 11:53:12 +0100

    Made the yamlFormat of the pipeline a Large Object Binary

--- datawaves-experimenter/src/main/java/vjabuilds/dev/models/Pipeline.java
+++ datawaves-experimenter/src/main/java/vjabuilds/dev/models/Pipeline.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
@@ -23,6 +24,7 @@ public class Pipeline extends BaseModel {
     private String description;
     private String name;
     private String version;
+    @Lob
     private String yamlFormat;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "parentPipeline")
     private List<Dataset> childDatasets;
